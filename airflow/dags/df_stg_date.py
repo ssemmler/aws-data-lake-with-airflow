d5c913072a88696b0eb3c67467601c97ac791fb9
Semmler, Stephan <stephan.semmler@axelspringer.de>
2018-11-30 17:35:20 +0100

    Update df_stg_date.py

--- airflow/dags/df_stg_date.py
+++ airflow/dags/df_stg_date.py
@@ -7,8 +7,13 @@ import random
 import airflow
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
+import boto3
 
+
+glue = boto3.client(service_name='glue', region_name='eu-west-1',
+              endpoint_url='https://glue.eu-west-1.amazonaws.com')
 
 default_args = {
     'owner': 'airflow',
@@ -27,17 +32,44 @@ default_args = {
 
 dag = DAG('df_stg_date', default_args=default_args, schedule_interval=None)
 
+def start_df_stg_date(ds, **kwargs):
+    response = glue.start_job_run(JobName='df_stg_date',Arguments={'--region': 'eu-west-1','--EFFECTIV_DATE': '2018-11-13'})
+    pprint(kwargs)
+    print(ds)
+    return response
+
 # t1, t2 and t3 are examples of tasks created by instantiating operators
-t1 = BashOperator(
-    task_id='df_stg_date',
-    bash_command='aws glue start-job-run  --job-name df_stg_date --region eu-west-1 --output text --arguments EFFECTIV_DATE=2018-11-12',
-    xcom_push=True,
-    dag=dag)
+#t1 = BashOperator(
+#    task_id='df_stg_date',
+#    bash_command='aws glue start-job-run  --job-name df_stg_date --region eu-west-1 --output text --arguments EFFECTIV_DATE=2018-11-12',
+#    xcom_push=True,
+#    dag=dag)
 
 t2 = BashOperator(
   task_id='df_stg_date_job_id',
-  bash_command="echo {{ ti.xcom_pull('df_stg_date') }}",
+  bash_command="echo {{ ti.xcom_pull('df_stg_date_2') }}",
+  retries=0,
+    print(ds)
+    return response
+
+# t1, t2 and t3 are examples of tasks created by instantiating operators
+#t1 = BashOperator(
+#    task_id='df_stg_date',
+#    bash_command='aws glue start-job-run  --job-name df_stg_date --region eu-west-1 --output text --arguments EFFECTIV_DATE=2018-11-12',
+#    xcom_push=True,
+#    dag=dag)
+
+t2 = BashOperator(
+  task_id='df_stg_date_job_id',
+  bash_command="echo {{ ti.xcom_pull('df_stg_date_2') }}",
   retries=0,
   dag=dag)
 
-t2.set_upstream(t1)
+t3 = PythonOperator(
+    task_id='start_df_stg_date_2',
+    provide_context=True,
+    python_callable=start_df_stg_date,
+    dag=dag,
+)
+
+t2.set_upstream(t3)
